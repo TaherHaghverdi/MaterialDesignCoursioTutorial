cb08766925280ff5cca1c881f3694645e6741e55
Taher <t.haghverdi@yahoo.com>
2016-11-30 06:02:30 +0330

    added an About Us page with material design

--- app/src/main/java/ir/coursio/materialtheme/AboutUsActivity.java
+++ app/src/main/java/ir/coursio/materialtheme/AboutUsActivity.java
@@ -2,6 +2,10 @@ package ir.coursio.materialtheme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.truizlop.fabreveallayout.FABRevealLayout;
 
 public class AboutUsActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        ImageView imgCancel = (ImageView) findViewById(R.id.imgCancel);
+        final FABRevealLayout fabRevealLayout = (FABRevealLayout) findViewById(R.id.fab_reveal_layout);
+
+        assert imgCancel != null;
+        imgCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                assert fabRevealLayout != null;
+                fabRevealLayout.revealMainView();
+
+            }
+        });
+
     }
 }
